865ae33a21ac84e2a30ed85af2a9e3281e48cc98
vipinmehrq535 <vipinsinghmehra535.gmail.com>
2021-12-08 00:38:49 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:foodie_ui_kit/pages/LoginPage.dart';
 import 'package:foodie_ui_kit/pages/root_app.dart';
+import 'router.dart' as router;
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
+    onGenerateRoute: router.generateRoute,
     home: LoginPage(),
   ));
 }
